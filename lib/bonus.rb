acc1bdeab1702fed15c05db311b9100d553c7289
Bobby BuffaloBoy <novenarii@gmail.com>
2019-06-17 06:04:32 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,12 +21,11 @@ def bonus
   }
 
 
-    epic_tragedy[:montague][:status] = "dead"
-    epic_tragedy[:capulet][:status] = "dead"
+    epic_tragedy[:montague][:hero][:status] = "dead"
+    epic_tragedy[:capulet][:heroine][:status] = "dead"
 
+return epic_tragedy
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
-end
-end
 end
